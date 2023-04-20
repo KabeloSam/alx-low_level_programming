9ad9f179440d912c876fba8a98d03612dcd6aa33
KabeloSam <kabelosammykay@gmail.com>
2023-04-20 12:47:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
+/* function that prints programming is like building a multingual puzzle*/
 int main() {
-  puts("Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
   return 0;
 }
