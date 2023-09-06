5e393a0b9a99035784e9596e770ca6ce830a1440
KabeloSam <kabelosammykay@gmail.com>
2023-09-06 11:25:11 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main-prints the name of the program
+*@argc:stores size of array
+*@argv: pointer to string array
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
