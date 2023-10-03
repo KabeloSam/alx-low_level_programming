bed5d00ec318c53269e9be7b68e9c25f0b6e2a37
KabeloSam <kabelosammykay@gmail.com>
2023-10-03 12:52:04 -0700

    0x15task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	my_file = open(filename, O_WRONLY | o_CREAT | O_TRUNC, 0600);
+	my_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (my_file == -1)
 	{
 	return (-1);
