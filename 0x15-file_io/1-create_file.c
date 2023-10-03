b98c3abfd3549bc1bf941dcf93e6dede0b9c67dd
KabeloSam <kabelosammykay@gmail.com>
2023-10-03 12:49:22 -0700

    0x15task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,21 +2,19 @@
 /**
  *create_file - creates a file
  *@filename: name of the file to be created
- *text_content:string to write to the file
+ *@text_content:string to write to the file
  *Return: returns 1 on success or -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int my_file, num_bytes;
-	int counter;
-	counter = 0;
-	my_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	
+	int my_file, num_bytes, counter = 0;
+
 	if (filename == NULL)
 	{
 		return (-1);
 	}
+	my_file = open(filename, O_WRONLY | o_CREAT | O_TRUNC, 0600);
 	if (my_file == -1)
 	{
 	return (-1);
@@ -27,17 +25,12 @@ int create_file(const char *filename, char *text_content)
 		{
 		counter++;
 		}
-	
 	num_bytes = write(my_file, text_content, counter);
-	
 	if (num_bytes == -1)
 	{
 		return (-1);
 	}
-	
 	}
 	close(my_file);
 	return (1);
-	
-	
 }
