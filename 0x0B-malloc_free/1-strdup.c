ed4515e66318fbd3ad4a6b4b3364de4f9c595f0f
KabeloSam <kabelosammykay@gmail.com>
2023-10-21 09:42:26 -0700

    malloctask1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_strdup- duplicates string
+ *@str : string to be duplicated
+ *Return: a pointer to the duplicated string or null if failed
+ */
+char *_strdup(char *str)
+{
+	char *new_str;
+
+	new_str = malloc(sizeof(char) * strlen(str));
+	
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(new_str, str);
+
+	return (new_str);
+
+
+}
